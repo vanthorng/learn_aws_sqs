9420762e0fc315bb096a85e13479aa757d3c36f5
vanthorng <t.vanthorng@casbiz.com.kh>
2026-09-08 16:05:20 +0700

    fix: set default queue to standard-queue and connection to sqs in imports and queue config

--- config/imports.php
+++ config/imports.php
@@ -2,8 +2,8 @@
 
 return [
     'disk' => env('IMPORTS_DISK', env('AWS_BUCKET') ? 's3' : 'local'),
-    'queue_connection' => env('IMPORT_QUEUE_CONNECTION', env('QUEUE_CONNECTION', 'database')),
-    'queue' => env('IMPORT_QUEUE', 'invoice-imports'),
+    'queue_connection' => env('IMPORT_QUEUE_CONNECTION', env('QUEUE_CONNECTION', 'sqs')),
+    'queue' => env('IMPORT_QUEUE', env('SQS_QUEUE', 'standard-queue')),
     'dead_letter_queue' => env('SQS_DLQ'),
     'batch_size' => (int) env('IMPORT_BATCH_SIZE', 100),
 ];
